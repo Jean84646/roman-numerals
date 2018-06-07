4fbaa93f05e3e4039a3a29b2b1c9881836ce24a4
Anousone Kaseumsouk, Jean Jia and Lan Dam <jean@>
2018-06-07 09:00:36 -0700

    Test first spec, it works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 // Business Logic
 var romanConverter = function(number){
-  return number;
+  if(number === 1){
+    return "I";
+  } else if(number === 5){
+    return "V";
+  } else if(number === 10){
+    return "X";
+  } else if(number === 50){
+    return "V";
+  } else if(number === 100){
+    return "C";
+  } else if(number === 500){
+    return "D";
+  } else if(number === 1000){
+    return "M";
+  } else {
+    return "Out Of Range";
+  };
 };
 
 
@@ -21,7 +37,7 @@ var romanConverter = function(number){
 $(document).ready(function(){
   $("#roman-form").submit(function(event){
     event.preventDefault();
-    var userInput = $("input#input-number").val();
+    var userInput = parseInt($("input#input-number").val());
     var result = romanConverter(userInput);
     $('#user-output').text(result);
   });
